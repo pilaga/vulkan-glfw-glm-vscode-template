7089a8e32713c2b5b0c809e9a6f4d2693990c197
Pierre <36122894+pilaga@users.noreply.github.com>
2023-12-16 16:52:50 +1100

    add function to create shader module from compiled shader bytecode

--- src/app.cpp
+++ src/app.cpp
@@ -80,7 +80,22 @@ class VulkanTemplateApp {
             auto fragShaderCode = readFile(Config::SHADERS_PATH + "frag.spv");
         }
 
-        VkShaderModule createShaderModule(const std::vector<char> &code) {}
+        /**
+         * Creates a shader module from the shader bytecode.
+         */
+        VkShaderModule createShaderModule(const std::vector<char> &code) {
+            VkShaderModuleCreateInfo create_info{};
+            create_info.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+            create_info.codeSize = code.size();
+            create_info.pCode = reinterpret_cast<const uint32_t *>(code.data());
+
+            VkShaderModule shader_module;
+            if (vkCreateShaderModule(device, &create_info, nullptr, &shader_module) != VK_SUCCESS) {
+                throw std::runtime_error("error: failed to create shader module!");
+            }
+
+            return shader_module;
+        }
 
         /*
          * Creates the GLFW window surface.
--- src/utils.cpp
+++ src/utils.cpp
@@ -24,7 +24,7 @@ std::vector<char> readFile(const std::string &filename) {
     std::ifstream file(filename, std::ios::ate | std::ios::binary);
 
     if (!file.is_open()) {
-        throw std::runtime_error("failed to open file!");
+        throw std::runtime_error("error: failed to open file!");
     }
 
     size_t file_size = (size_t)file.tellg();
