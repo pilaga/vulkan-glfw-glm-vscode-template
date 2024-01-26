57a86419ea44267bffe3f4838b71cbc16889f88f
Pierre <36122894+pilaga@users.noreply.github.com>
2024-01-27 09:58:12 +1100

    move vertex attribute description to the vertex header, add attribute descriptions for position and color

---
+++ include/vertex.hpp
@@ -0,0 +1,53 @@
+#ifndef _VERTEX_H_
+#define _VERTEX_H_
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <array>
+#include <fstream>
+#include <glm/glm.hpp>
+#include <iostream>
+#include <optional>
+#include <string>
+#include <vector>
+
+/**
+ * Vertex input description.
+ */
+struct Vertex {
+        glm::vec2 pos;
+        glm::vec3 color;
+
+        // Vertex binding describes at which rate to load data from memory through the vertices
+        static VkVertexInputBindingDescription getBindingDescription() {
+            VkVertexInputBindingDescription binding_desc{};
+            binding_desc.binding = 0;
+            binding_desc.stride = sizeof(Vertex);
+            binding_desc.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;  // Move to the next data point after each vertex
+
+            return binding_desc;
+        }
+
+        // Vertex input attributes describes the format of a vertex within a data chunk
+        // A vertex consists of 2 data points (position and color) so we initialize a list
+        static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
+            std::array<VkVertexInputAttributeDescription, 2> attribute_desc_list{};
+
+            // Attribute description for position
+            attribute_desc_list[0].binding = 0;
+            attribute_desc_list[0].location = 0;                      // Location directive of the input in the vertex shader
+            attribute_desc_list[0].format = VK_FORMAT_R32G32_SFLOAT;  // Format matching vec2
+            attribute_desc_list[0].offset = offsetof(Vertex, pos);
+
+            // Attribute description for color
+            attribute_desc_list[1].binding = 0;
+            attribute_desc_list[1].location = 1;                         // Location in the vertex shader
+            attribute_desc_list[1].format = VK_FORMAT_R32G32B32_SFLOAT;  // Format matching vec3
+            attribute_desc_list[1].offset = offsetof(Vertex, color);
+
+            return attribute_desc_list;
+        }
+};
+
+#endif  // _VERTEX_H_
